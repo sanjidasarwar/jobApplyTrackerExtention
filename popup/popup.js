d23773573c4593122cb7cfadc0e86b630baa27f3
tupsi07 <sanjida657@yahoo.com>
2026-04-21 11:55:00 +0600

    feat: display saved jobs in popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,4 @@
+// save job data when user clicks "Save Job" button in popup
 document.getElementById("saveBtn").addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     console.log(tab)
@@ -13,8 +14,15 @@ document.getElementById("saveBtn").addEventListener("click", async () => {
             console.log(jobs)
             const newJob ={
                 ...response,
-                status: "Applied",
+                status: "Saved",
                 date: new Date().toISOString()
+            }
+            
+            // check for duplicates
+            const isAlreadySaved = jobs.some(job => job.url === newJob.url);
+            if (isAlreadySaved) {
+                alert("This job is already saved!");
+                return;
             }
 
             jobs.push(newJob);
@@ -25,3 +33,27 @@ document.getElementById("saveBtn").addEventListener("click", async () => {
     })
 })
 
+// load saved jobs when popup opens
+
+document.addEventListener("DOMContentLoaded", loadJobs);
+
+function loadJobs() {
+    chrome.storage.local.get(["jobs"], (data) => {
+        const jobs = data.jobs || [];
+        const jobContainer = document.getElementById("jobs");
+        jobContainer.innerHTML = "";
+
+        jobs.forEach((job, index) => {
+            const jobDiv = document.createElement("div");
+            jobDiv.className = "job-card";
+            jobDiv.innerHTML =`
+                <h3>${job.title}</h3>
+                <a href="${job.url}" target="_blank">View Job</a>
+                <p>Status: ${job.status}</p>
+                <p>Applied on: ${new Date(job.date).toLocaleDateString()}</p>
+            `
+            jobContainer.appendChild(jobDiv);
+        })
+
+    })
+}
